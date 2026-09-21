7a89588534ed4add411a6a1bcdde8f4e697b282a
Vasil Jovev <vasevase961@gmail.com>
2026-09-21 09:49:07 +0200

    fix: keep custom player fullscreen on iOS

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,12 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
